1855a543db52737dbbc6dfe91e846008f882553d
Julie <MsMutinda@users.noreply.github.com>
2021-04-22 13:07:50 +0300

    Heroku commit 2 hp

--- covidTracker/settings.py
+++ covidTracker/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'HOST': "127.0.0.1",
         'PORT': "",
         'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
             },
         }
     }
