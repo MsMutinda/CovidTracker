deb14faa01a11331ef149944e9adcc9bf8c549e8
Julie <MsMutinda@users.noreply.github.com>
2021-04-22 13:20:41 +0300

    Heroku commit 2 hp

--- covidTracker/settings.py
+++ covidTracker/settings.py
@@ -73,7 +73,7 @@ WSGI_APPLICATION = 'covidTracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'covidTracker',
         'USER': 'root',
         'PASSWORD': "",
