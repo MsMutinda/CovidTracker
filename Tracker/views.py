14240a9504587820e202494a5d9e7dd5df9aa295
Julie <MsMutinda@users.noreply.github.com>
2021-04-28 10:37:37 +0300

    stats plot

--- Tracker/views.py
+++ Tracker/views.py
@@ -17,8 +17,8 @@ def stats(request):
     resp = json.dumps(requests.get('https://api.covid19api.com/summary').json(), sort_keys=True, indent=4)
     respdata1 = json.loads(resp)
     plotsdata = {
-        'Date': respdata1['Global'].Date,
-        'New Confirmed Cases': respdata1['Global'].NewConfirmed,
+        'Date': respdata1['Global']['Date'],
+        'New Confirmed Cases': respdata1['Global']['NewConfirmed'],
         'New Deaths': respdata1.Global.NewDeaths,
         'New Recoveries': respdata1.Global.NewRecovered,
         'Total Confirmed': respdata1.Global.TotalConfirmed,
